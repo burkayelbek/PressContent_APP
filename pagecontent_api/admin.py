6fa0fe7e4b462df345e97d90746dbce3e4ec61fa
burkayelbek <burkay.elbek@gmail.com>
2022-11-12 15:20:48 +0300

    Models creatad & test method created for views

--- pagecontent_api/admin.py
+++ pagecontent_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from pagecontent_api.models import PressContent
 
-# Register your models here.
+@admin.register(PressContent)
+class CommentAdmin(admin.ModelAdmin):
+    search_fields = ['date']
+    list_display = ['pk', 'label','url','date','created','modified']
--- pagecontent_api/views.py
+++ pagecontent_api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def test_api(self):
     return HttpResponse("Hello World")
