85c2d2010c465ee0cff7e7b2deb63b2a8f9ba9a1
burkayelbek <burkay.elbek@gmail.com>
2022-11-12 15:32:19 +0300

    Settings and requirements edited

--- config/settings.py
+++ config/settings.py
@@ -30,6 +30,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    # Custom APPS
     'pagecontent_api'
 ]
 
