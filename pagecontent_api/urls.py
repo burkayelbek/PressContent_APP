a3547f0f110dad76aaf1195bb71dee916cc4d1c9
burkayelbek <burkay.elbek@gmail.com>
2022-11-12 17:20:01 +0300

    Url slugify created in model

--- pagecontent_api/urls.py
+++ pagecontent_api/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import RedirectView
 from pagecontent_api.views import PressContentListAPIView, PressContentDetailAPIView
 
 urlpatterns = [
-    path('',  RedirectView.as_view(pattern_name='homepage')),
+    path('',  RedirectView.as_view(pattern_name='list')),
     path('list/', PressContentListAPIView.as_view(), name="list"),
     path('detail/<pk>', PressContentDetailAPIView.as_view(), name='detail')
 ]
