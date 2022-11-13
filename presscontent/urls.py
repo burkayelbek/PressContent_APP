cf9ad25aa8337ff36546956ad228b78a1fe86281
burkayelbek <burkay.elbek@gmail.com>
2022-11-13 17:45:40 +0300

    README.md file edited & in urls.py removed redundant path

--- presscontent/urls.py
+++ presscontent/urls.py
@@ -8,8 +8,6 @@ app_name = "presscontent"
 
 
 urlpatterns = [
-    #path('',  RedirectView.as_view(pattern_name='list')),
-    # path('list/', PressContentListAPIView.as_view({'get':'list'}), name="list"),
     path('list/', PressContentListAPIView.as_view(), name="list"),
     path('detail/<slug:slug>', PressContentDetailAPIView.as_view(), name='detail')
 
